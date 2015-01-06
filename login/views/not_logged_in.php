5045f8c6d1fa9588f841d1afb2c7b1c42417b969
Mike Barnard <michael.d.barnard@gmail.com>
2015-01-06 18:48:27 -0500

    Moved login script

---
+++ login/views/not_logged_in.php
@@ -0,0 +1,78 @@
+<?php
+
+// show negative messages
+if ($login->errors) {
+    foreach ($login->errors as $error) {
+        echo $error;    
+    }
+}
+
+// show positive messages
+if ($login->messages) {
+    foreach ($login->messages as $message) {
+        echo $message;
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en" manifest="manifest.appcache">
+  <head>
+    <meta charset="utf-8">
+    <title>OvR Trip Lists</title>
+
+    <!-- Mobile view properties & enable iOS Web App-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+
+    <!-- favicon and apple-touch-icon -->
+    <link rel="apple-touch-icon" href="../../assets/images/touch-icon-iphone.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="../../assets/images/touch-icon-ipad.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="../../assets/images/touch-icon-iphone-retina.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="../../assets/images/touch-icon-ipad-retina.png" />
+    <link rel="icon" type="image/png" href="http://ovrride.com/favicon.ico">
+
+    <!-- Include compiled and minified stylesheets -->
+    <link rel="stylesheet" href="../../assets/stylesheets/all.css">
+    <!-- Include tablesorter styles -->
+    <link rel="stylesheet" href="../../assets/tablesorter/css/theme.bootstrap.css">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container login-window">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3">
+          <div class="panel panel-info">
+            <div class="panel-heading">
+              <h3 class="panel-title center-me">
+                <img src="../../assets/images/logo.jpg" align="middle">
+              </h3>
+            </div>
+            <div class="panel-body">
+              <form accept-charset="UTF-8" role="form" method="post" action="index.php" name="loginform">
+                <fieldset>
+                  <div class="form-group">
+                    <input id="login_input_username" class="form-control login_input" placeholder="Username" name="user_name" type="text" required />
+                  </div>
+                  <div class="form-group">
+                    <input id="login_input_password" class="form-control login_input" placeholder="Password" name="user_password" type="password" autocomplete="off" required />
+                  </div>
+                  <input class="btn btn-lg btn-primary btn-block" type="submit" name="login" value="Log in">
+                </fieldset>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Include concatenated and minified javascripts -->
+    <script src="../../assets/javascripts/all.min.js"></script>
+  </body>
+</html>
